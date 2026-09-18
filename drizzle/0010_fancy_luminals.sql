74c40b79ec39e2039bd6096d2e0d2f9e7d5098a5
wenlongxiang9-jpg <wenlongxiang9@gmail.com>
2026-09-18 20:42:17 +0800

    feat: 兼容历史销售数据的增量数据库迁移

---
+++ drizzle/0010_fancy_luminals.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `sales_records` ADD `currency` text;--> statement-breakpoint
+ALTER TABLE `sales_records` ADD `reported_amount` real;--> statement-breakpoint
+ALTER TABLE `sales_records` ADD `ad_cost` real;
